7f9ae870d096ca0998695e01c8b26996a4dc4830
Fran G <frances.h.gray@gmail.com>
2024-10-30 17:49:59 -0700

    enabled custom access logging for the websocket and http gateways

--- lib/stacks/HttpGWStack.ts
+++ lib/stacks/HttpGWStack.ts
@@ -5,6 +5,9 @@ import {
   CfnOutput,
   Stack,
   StackProps,
+  aws_logs,
+  RemovalPolicy,
+  aws_iam,
 } from "aws-cdk-lib";
 import { DynamoLoggingStack } from "./DynamoLoggingStack";
 
@@ -32,11 +35,39 @@ export class HttpGWStack extends Stack {
       apiName: `HttpApi-${stageName}`,
     });
 
+    // creating log group for access logs
+    const logGroup = new aws_logs.LogGroup(this, "HTTPGatewayAccessLogs", {
+      logGroupName: `HTTPGatewayAccessLogs-${stageName}`,
+      removalPolicy: RemovalPolicy.DESTROY,
+      retention: aws_logs.RetentionDays.ONE_MONTH,
+    });
+
+    // create a role for API Gateway
+    const apiGatewayRole = new aws_iam.Role(this, "ApiGatewayLoggingRole", {
+      assumedBy: new aws_iam.ServicePrincipal("apigateway.amazonaws.com"),
+    });
+
+    // Grant API Gateway permissions to write to Cloudwatch logs
+    logGroup.grantWrite(apiGatewayRole);
+
     // set stage
     const stage = new aws_apigatewayv2.CfnStage(this, `${stageName}`, {
       apiId: httpApi.httpApiId,
       stageName: stageName,
       autoDeploy: true,
+      accessLogSettings: {
+        destinationArn: logGroup.logGroupArn,
+        format: JSON.stringify({
+          requestId: "$context.requestId",
+          userAgent: "$context.identity.userAgent",
+          sourceIp: "$context.identity.sourceIp",
+          requestTime: "$context.requestTime",
+          httpMethod: "$context.httpMethod",
+          path: "$context.path",
+          status: "$context.status",
+          responseLength: "$context.responseLength",
+        }),
+      },
     });
 
     // post route for cdkTest
